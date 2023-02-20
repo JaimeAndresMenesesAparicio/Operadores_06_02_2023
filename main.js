557715990a8feef0a323e1a61c1ef79c414ccbdc
JaimeAndresMenesesAparicio <jaime.meneses89@gmail.com>
2023-02-20 18:28:02 -0500

    Operador lógico 'OR ||'

--- main.js
+++ main.js
@@ -6,9 +6,9 @@
 let a = confirm("Variable A\n\t\tSelecciona Aceptar si deseas el booleano True\n\t\tSelecciona Cancelar si deseas el booleano False");
 let b = confirm("Variable B\n\t\tSelecciona Aceptar si deseas el booleano True\n\t\tSelecciona Cancelar si deseas el booleano False")
 
-let cuerpo = document.querySelector("#myAND");
+let cuerpo = document.querySelector("#myOR");
 cuerpo.insertAdjacentHTML("beforeend", `<tr>
                                             <td>${a}</td>
                                             <td>${b}</td>
-                                            <td>${a && b}</td>
+                                            <td>${a || b}</td>
                                         </tr>`)
